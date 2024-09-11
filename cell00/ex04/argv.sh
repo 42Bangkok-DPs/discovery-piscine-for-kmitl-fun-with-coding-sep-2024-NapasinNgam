7f42ca74c8b07131061843e257d6db7b9d0ff473
NapasinNgam <154305404+NapasinNgam@users.noreply.github.com>
2024-09-11 15:47:23 +0700

    Add files via upload

---
+++ cell00/ex04/argv.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+
+if [ "$#" -eq 0 ]; then
+    echo "No arguments supplied"
+elif [ "$#" <= 3 ] ; then 
+    for arg in "$@"; do 
+       echo "$arg"
+    done
+elif [ "$#" -gt 3 ]; then
+    echo $1;
+    echo $2;
+    echo $3;
+fi
+
+
